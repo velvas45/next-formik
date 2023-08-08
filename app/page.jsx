3ff259762a5b915c967c9d022360326a383182b4
helmi.agustiawan <helmi.agustiawan@JKT-MAC-2114.local>
2023-08-08 10:28:54 +0700

    chore(create-project): first commit for setup project

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <form className="bg-white flex rounded-lg w-1/2">
+        <div className="flex-1 text-gray-700 p-20">
+          <h1>let's get started </h1>
+          <p>
+            Join our E-learning platform today and unlock over 500+ courses and
+            digital assets ready to download
+          </p>
+        </div>
+        <div></div>
+      </form>
+    </main>
+  );
+}
